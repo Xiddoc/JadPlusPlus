cba3ad1f8a1209737b3d9e7e0acf91fb09fc477d
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2023-09-24 19:25:26 +0300

    Implemented parsing of overall Jadx project (not complete)

--- jadx/JadxProject.py
+++ jadx/JadxProject.py
@@ -29,6 +29,16 @@ class JadxProject(ProjectComponent):
     def from_json(parsed_json: Dict[str, Any]) -> "ProjectComponent":
         return JadxProject(
             project_version=parsed_json["projectVersion"],
+
+            files=parsed_json["files"],
+            tree_expansions=parsed_json["treeExpansions"],
+            code_data=parsed_json["codeData"],
+            open_tabs=parsed_json["openTabs"],
+
+            active_tab=parsed_json["activeTab"],
+            cache_dir=parsed_json["cacheDir"],
+            enable_live_reload=parsed_json["enableLiveReload"],
+            search_history=parsed_json["searchHistory"]
         )
 
     def to_json(self) -> str:
